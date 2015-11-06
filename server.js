985fe3e44169209125ee6f5266363509c77055aa
Antiej <anton.jidkov@gmail.com>
2015-11-06 15:55:06 +0000

    port update

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ var express = require('express'),
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', wines.index);
 app.get('/wines', wines.findAll);
 app.get('/wines/:id', wines.findById);
 
-app.listen(5000);
-console.log('Listening on port 5000...');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
