c7feb9c7ef59e17236c43094b44300f5b114faea
Antiej <anton.jidkov@gmail.com>
2015-11-06 16:58:35 +0000

    db endpoints

--- server.js
+++ server.js
@@ -5,18 +5,47 @@ var express = require('express'),
 
 var uri = 'mongodb://heroku_g8hjpjnj:1chs4grt922vnn8iroaf4aam03@ds053160.mongolab.com:53160/heroku_g8hjpjnj';
 
+var seedData = [
+  {
+    aj1: 'hello1'
+  },
+  {
+    aj2: 'hello2'
+  }
+];
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', wines.index);
-app.get('/db', function(req, res){
+app.get('/dbinsert', function(req, res){
 	mongo.MongoClient.connect(uri, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server");
-  db.close();
+
+  var collection = db.collection('ajcollection');
+  collection.insert(seedData, function(err, result) {
+    
+    if(err) throw err;
+    db.close();
+});  
 });
-	res.send("connected to server");
+	res.send("db");
+});
+
+app.get('/dbfind', function(req, res){
+	mongo.MongoClient.connect(uri, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  var collection = db.collection('ajcollection');
+  
+collection.find({}).toArray(function(err, docs) {
+    res.send(docs);
+  }); 
+  
+});
 });
 
 
