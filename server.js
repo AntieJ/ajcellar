f78145ed2882fcad897d1a5f9db94b2c797f4995
Antiej <anton.jidkov@gmail.com>
2015-11-06 15:51:34 +0000

    port change

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.get('/', wines.index);
 app.get('/wines', wines.findAll);
 app.get('/wines/:id', wines.findById);
 
-app.listen(3000);
-console.log('Listening on port 3000...');
+app.listen(5000);
+console.log('Listening on port 5000...');
