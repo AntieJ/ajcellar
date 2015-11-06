b90cd3761ce8400b0526ebcfd8e68c133eaffc4c
Antiej <anton.jidkov@gmail.com>
2015-11-06 16:14:44 +0000

    db test

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 var express = require('express'),
-    wines = require('./routes/wines');
+    wines = require('./routes/wines'),
+    mongo = require('mongodb').
+    assert = require('assert');
+
+var MongoClient = mongo.MongoClient;
+var uri = 'mongodb://heroku_g8hjpjnj:1chs4grt922vnn8iroaf4aam03@ds053160.mongolab.com:53160/heroku_g8hjpjnj';
 
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', wines.index);
+app.get('/db', function(req, res){
+	MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  db.close();
+});
+});
+
+
+
 app.get('/wines', wines.findAll);
 app.get('/wines/:id', wines.findById);
 
